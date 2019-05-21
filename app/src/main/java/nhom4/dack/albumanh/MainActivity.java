ad82c81a61eeb1fdc78024ad5b06aec9e91f6e7f
beohoang98 <beohoang98@users.noreply.github.com>
2019-05-21 17:45:31 +0700

    home layout

--- app/src/main/java/nhom4/dack/albumanh/MainActivity.java
+++ app/src/main/java/nhom4/dack/albumanh/MainActivity.java
@@ -3,12 +3,30 @@ package nhom4.dack.albumanh;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setElevation(0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    private void mapViewIntoVariablle()
+    {
+//        toolbar = findViewById(R.id.toolbar);
     }
 }
